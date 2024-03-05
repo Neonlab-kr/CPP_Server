30d4a7c84d26565f4a79d5d16bc32f1e803e1b1c
Neonlab_kr <ksknk67@gmail.com>
2024-03-05 17:49:56 +0900

    Lock Basic

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,40 +3,67 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-//atomic : All-Or-Nothing
-atomic<int32> sum = 0;
+vector<int32> v;
 
-void Add()
+//Mutual Exclusive(상호배타적)
+mutex m;
+
+//RAII (Resource Acquisition is Initainlization)
+template<typename T>
+class LockGuard
 {
-	for (int32 i = 0; i < 100'0000; i++)
+public:
+	LockGuard(T& m)
 	{
-		sum.fetch_add(1);
-		//sum++;
+		_mutex = &m;
+		_mutex->lock();
+	}
+
+	~LockGuard()
+	{
+		_mutex->unlock();
+	}
+
+private:
+	T* _mutex;
+};
+
+void Push()
+{
+	for (int32 i = 0; i < 10000; i++)
+	{
+		//자물쇠 잠그기
+		//LockGuard<std::mutex> lockGuard(m);
+		std::lock_guard<std::mutex> lockGuard(m);
+		//std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock);
+		//uniqueLock.lock();
+
+		//m.lock();
+		//m.lock();
+
+		v.push_back(i);
+
+		if (i == 5000)
+		{
+			//m.unlock();
+			break;
+		}
+
+		//자물쇠 풀기
+		//m.unlock();
+		//m.unlock();
 	}
 }
-
-void Sub()
-{
-	for (int32 i = 0; i < 100'0000; i++)
-	{
-		sum.fetch_add(-1);
-		//sum--;
-	}
-}
-
 
 int main()
 {
-	Add();
-	Sub();
-	cout << sum << endl;
+	std::thread t1(Push);
+	std::thread t2(Push);
 
-	std::thread t1(Add);
-	std::thread t2(Sub);
 	t1.join();
 	t2.join();
 
-	cout << sum << endl;
-
+	cout << v.size() << endl;
 }
