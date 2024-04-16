2cab48156024696a09992fc5eb67566f0c8618d6
Neonlab_kr <ksknk67@gmail.com>
2024-04-16 19:44:29 +0900

    Thread Local Storage

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,53 +6,30 @@
 #include <windows.h>
 #include <future>
 
-atomic<bool> ready;
-int32 value;
+//__declspec(thread) int32 value;
+thread_local int32 LThreadid = 0;
 
-void Producer()
+void ThreadMain(int32 threadid)
 {
-	value = 0;
+	LThreadid = threadid;
 
-	ready.store(true);
-}
-
-void Consumer()
-{
-	while (ready.load()==false)
-		;
-
-	cout << value << endl;
+	while (true)
+	{
+		cout << "Hi! I am Thread " << LThreadid << endl;
+		this_thread::sleep_for(1s);
+	}
 }
 
 int main()
 {
-	ready = false;
-	value = 0;
-	thread t1(Producer);
-	thread t2(Consumer);
-	t1.join();
-	t2.join();
+	vector<thread> threads;
 
-	//Memory Model (정책)
-	// 1) Sequentially Consistent (seq_cst)
-	// 2) Acquire-Release (consume, acquire, release, acq_rel)
-	// 3) Released (relaxed)
+	for (int32 i = 0; i < 10; i++)
+	{
+		int32 threadid = i + 1;
+		threads.push_back(thread(ThreadMain, threadid));
+	}
 
-	// 1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
-	// 가시성 문제 바로 해결, 코드 재배치 바로 해결
-	
-	// 2) acquire-release
-	// 딱 중간
-	// release 명령 이전의 메모리 명령들이, 해당 명령 이후로 재배치 되는것을 막아줌.
-	// 그리고 acquire로 같은 변수를 읽는 스레드가 있다면
-	// release 이전의 명령들이 -> acquire 하는 순간에 관찰 가능(가시성 보장)
-	
-	// 3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
-	// 너무나도 자유롭다!
-	// 코드 재배치도 멋대로 가능! 가시성 해결 NO
-	// 가장 기본 조건 (동일 객체에 대한 동일 관찰 순서)
-
-	// 인텔, AMD의 경우 애당초 순차적 일관성을 보장함.
-	// seq_cst를 써도 별다른 부하가 없음
-	// ARM의 경우 꽤 차이가 있음.
+	for (thread& t : threads)
+		t.join();
 }
