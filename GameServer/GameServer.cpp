84049e06c948d4dd7dbe1c5ed272d956a8874cab
Neonlab_kr <ksknk67@gmail.com>
2024-04-30 17:25:30 +0900

    Lock-Free Stack #3

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -18,7 +18,7 @@ void Push()
 		int32 value = rand() % 100;
 		s.Push(value);
 
-		this_thread::sleep_for(10ms);
+		this_thread::sleep_for(1ms);
 	}
 }
 
@@ -26,9 +26,9 @@ void Pop()
 {
 	while (true)
 	{
-		int32 data = 0;
-		if (s.TryPop(OUT data))
-			cout << data << endl;
+		auto data = s.TryPop();
+		if (data != nullptr)
+			cout << (*data) << endl;
 	}
 }
 
