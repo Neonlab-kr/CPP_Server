2bfd2302b358c1f82cfb88ba49bcd0d1aabd8cad
Neonlab-kr <92604834+Neonlab-kr@users.noreply.github.com>
2025-12-03 22:27:31 +0900

    12/03

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,36 +4,57 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <future>
 
-vector<int32> v;
-mutex m;
+#include <windows.h>
 
+atomic<bool> ready;
+int32 value;
 
-
-void Push()
+void Producer()
 {
-	for (int32 i = 0; i < 1'0000; i++)
-	{
-		//RAII 패턴(생성자에서 lock 소멸자에서 unlock)
-		//std::lock_guard<std::mutex> lockGuard(m);//생성하면 잠기고 소멸시 해제
-		std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock); //만들자마자 잠그는게 아니라 실제로 lock시 잠금, 락가드보다 조금 무거움
+	value = 10;
 
-		uniqueLock.lock();
-		//m.lock();
+	ready.store(true, memory_order::memory_order_seq_cst);
+}
 
-		v.push_back(i);
-
-		//m.unlock();
-	}
+void Consumer()
+{
+	while (ready.load(memory_order::memory_order_seq_cst) == false);
+	
+	cout << value << endl;
 }
 
 int main()
 {
-	std::thread t1(Push);
-	std::thread t2(Push);
-
+	ready = false;
+	value = 0;
+	thread t1(Producer);
+	thread t2(Consumer);
 	t1.join();
 	t2.join();
 
-	cout << v.size()<< endl;
+	/*
+	[Memory Model(메모리 정책)]
+
+	1) Sequentially Consistent (seq_cst)
+	2) Acquire-Release ((consume), acquire, release, acq_rel)
+	3) Relaxed (relaxed)
+
+	1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
+	- 가시성 문제 해결, 코드 재배치 바로 해결
+
+	2) acquire-release ()
+	- 딱 중간
+	- acquire와 release가 한쌍
+	- release 명령 이전의 메모리 명령들이, 해당 명령 이후로 재배치 되는 것을 금지
+	- 그리고 acquire로 같은 변수를 읽는 쓰레드가 있다면, release 이전의 명령들이 acquire 하는 순간에 관찰 가능
+	=> 가시성 보장
+
+	3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
+	- 재배치 맘대로 가시성 해결 NO
+	- 가장 기본 조건 (동일 객체에 대한 동일 관전 순서만 보장)
+
+	*/
+	
 }
