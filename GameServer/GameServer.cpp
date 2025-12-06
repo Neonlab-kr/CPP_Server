e292154f7fdd8cd4a7ad2b5a9685722162b1831e
Neonlab-kr <92604834+Neonlab-kr@users.noreply.github.com>
2025-12-06 19:01:05 +0900

    12/06

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,53 +8,40 @@
 
 #include <windows.h>
 
-atomic<bool> ready;
-int32 value;
+#include "ConcurrentQueue.h"
+#include "ConcurrentStack.h"
 
-void Producer()
+LockQueue<int32> q;
+LockStack<int32> s;
+
+void Push()
 {
-	value = 10;
+	while (true)
+	{
+		int32 value = rand() % 100;
+		q.Push(value);
 
-	ready.store(true, memory_order::memory_order_seq_cst);
+		this_thread::sleep_for(1ms);
+	}
 }
 
-void Consumer()
+void Pop()
 {
-	while (ready.load(memory_order::memory_order_seq_cst) == false);
-	
-	cout << value << endl;
+	while (true)
+	{
+		int32 data = 0;
+		if(q.TryPop(OUT data))
+			cout << data << endl;
+	}
 }
 
 int main()
 {
-	ready = false;
-	value = 0;
-	thread t1(Producer);
-	thread t2(Consumer);
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
+
 	t1.join();
 	t2.join();
-
-	/*
-	[Memory Model(메모리 정책)]
-
-	1) Sequentially Consistent (seq_cst)
-	2) Acquire-Release ((consume), acquire, release, acq_rel)
-	3) Relaxed (relaxed)
-
-	1) seq_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
-	- 가시성 문제 해결, 코드 재배치 바로 해결
-
-	2) acquire-release ()
-	- 딱 중간
-	- acquire와 release가 한쌍
-	- release 명령 이전의 메모리 명령들이, 해당 명령 이후로 재배치 되는 것을 금지
-	- 그리고 acquire로 같은 변수를 읽는 쓰레드가 있다면, release 이전의 명령들이 acquire 하는 순간에 관찰 가능
-	=> 가시성 보장
-
-	3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
-	- 재배치 맘대로 가시성 해결 NO
-	- 가장 기본 조건 (동일 객체에 대한 동일 관전 순서만 보장)
-
-	*/
-	
+	t3.join();
 }
