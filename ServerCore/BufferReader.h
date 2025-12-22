48a4d132d8feb95ac9b2493436322512854bb8ef
Neonlab-kr <92604834+Neonlab-kr@users.noreply.github.com>
2025-12-22 17:30:00 +0900

    12/21

--- ServerCore/BufferReader.h
+++ ServerCore/BufferReader.h
@@ -9,7 +9,7 @@ public:
 	BufferReader(BYTE* buffer, uint32 size, uint32 pos = 0);
 	~BufferReader();
 
-	BYTE*			Buffer() { return _buffer; }
+	BYTE* Buffer() { return _buffer; }
 	uint32			Size() { return _size; }
 	uint32			ReadSize() { return _pos; }
 	uint32			FreeSize() { return _size - _pos; }
